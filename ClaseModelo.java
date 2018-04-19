7932c3a68ebc4a69e2b9e6d1ef1930ffb6e3dec5
Victor <38298615+rolavictor@users.noreply.github.com>
2018-04-19 09:00:08 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,29 +1,5 @@
 public class ClaseModelo {
-	 // Implementa la lógica del objetivo principal del programa 
+	 // Implementa la lÃ³gica del objetivo principal del programa 
 	
-		public int sumarDosEnteros(String n1, String n2) {
-			 int numero1 = Integer.parseInt(n1); 
-			 int numero2 = Integer.parseInt(n2);
-			 int resultado = numero1 + numero2;
-			 return (resultado);
-		 }
-		public int restarDosEnteros(String n1, String n2) {
-			 int numero1 = Integer.parseInt(n1); 
-			 int numero2 = Integer.parseInt(n2);
-			 int resultado = numero1 - numero2;
-			 return (resultado);
-		 }
-		public int multiplicarDosEnteros(String n1, String n2) {
-			 int numero1 = Integer.parseInt(n1); 
-			 int numero2 = Integer.parseInt(n2);
-			 int resultado = numero1*numero2;
-			 return (resultado);
-		 }
-		public int dividirDosEnteros(String n1, String n2) {
-			 int numero1 = Integer.parseInt(n1); 
-			 int numero2 = Integer.parseInt(n2);
-			 int resultado = numero1 / numero2;
-			 return (resultado);
-		 }
-	}
+}
 	
