8c9c3103238a8e3430a2bc3458e746e78f208936
Victor <38298615+rolavictor@users.noreply.github.com>
2018-04-19 08:59:14 +0200

    Update ClaseControlador.java

--- ClaseControlador.java
+++ ClaseControlador.java
@@ -1,52 +1,24 @@
- import java.awt.event.ActionEvent; 
- import java.awt.event.ActionListener;
- 
- public class ClaseControlador implements ActionListener {
-	 
-	 ClaseModelo objModelo = null;
-	 ClaseVista objVista = null;
-	
-	 public ClaseControlador(ClaseVista objVista, ClaseModelo objModelo) { 
-		 this.objModelo = objModelo; 
-		 this.objVista = objVista; 
-		 actionListener(this); // Escuchador para el botón 
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener;
+
+public class ClaseControlador implements ActionListener {
+
+	ClaseModelo objModelo = null;
+	ClaseVista objVista = null;
+
+	public ClaseControlador(ClaseVista objVista, ClaseModelo objModelo) { 
+		this.objModelo = objModelo; 
+		this.objVista = objVista; 
+		
 	}
- 
 
-	@Override 
-	public void actionPerformed(ActionEvent objEvento) { 
-		try {
-			int resultado = 0;
-			// Capturamos los números desde la vista 
-			String numero1 = this.objVista.cajaDeTextoNumero1.getText(); 
-			String numero2 = this.objVista.cajaDeTextoNumero2.getText();
-			
-			if(objEvento.getSource() == objVista.botonMultiplicar) {
-				resultado = objModelo.multiplicarDosEnteros(numero1, numero2);
-			} else if (objEvento.getSource() == objVista.botonSumar) {
-				resultado = objModelo.sumarDosEnteros(numero1, numero2);
-			} else if (objEvento.getSource() == objVista.botonRestar) {
-				resultado = objModelo.restarDosEnteros(numero1, numero2);
-			} else  {
-				resultado = objModelo.dividirDosEnteros(numero1, numero2);
-			}
-			objVista.cajaDeTextoResultado.setText( "" + resultado );
-			// Mostramos el resultado en la vista (GUI) 
-			 
-
-		}
-		catch(Exception objExcepcion) {
-			objExcepcion.printStackTrace();
-		}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
-   public void actionListener(ActionListener escuchador) {
-	 objVista.botonSumar.addActionListener(escuchador);
-	 objVista.botonRestar.addActionListener(escuchador);
-	 objVista.botonMultiplicar.addActionListener(escuchador);
-	 objVista.botonDividir.addActionListener(escuchador);
-	 }
- }
+}
 
- 	
- 	
- 
+
+
+
