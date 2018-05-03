e28e12956947edd7e83d4ffe36470b5626e16882
Victor <38298615+rolavictor@users.noreply.github.com>
2018-05-03 10:16:07 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,5 +1,23 @@
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 public class ClaseModelo {
 	 // Implementa la lógica del objetivo principal del programa 
-	
+	public void bloque(JButton h){
+		h.addMouseListener(new java.awt.event.MouseAdapter() { 
+			public void mouseClicked(java.awt.event.MouseEvent e) { 
+				int a=(int) ((3-0+1)*Math.random()+0);
+				h.setText(""+a);
+				if(a==0) {
+					//si responde si, seguir jugando. si pone otra cosa que acabe el programa
+					int dialogButton = JOptionPane.YES_NO_OPTION;
+					JOptionPane.showMessageDialog(null, "Has perdido");
+		            System.exit(0);
+		          
+				}
+			}
+		}
+				);
+	}
 }
 	
