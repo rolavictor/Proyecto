adc36a0ecdc058b9d63dc30ed15d8158f3917961
Victor <38298615+rolavictor@users.noreply.github.com>
2018-05-08 10:36:14 +0200

    Update ClasePrincipal.java

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -1,4 +1,3 @@
-
 public class ClasePrincipal {
 	
 	public static void main(String args[]) {
@@ -8,5 +7,3 @@ public class ClasePrincipal {
 		 new ClaseControlador(vista, modelo); 
 		 } 
 	}
-
-  
