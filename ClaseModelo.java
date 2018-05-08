83817cd70ffd1c34eb1b185a6c4e1438c30742e3
Victor <38298615+rolavictor@users.noreply.github.com>
2018-05-08 10:56:46 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -2,11 +2,12 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 public class ClaseModelo {
+	
 	 // Implementa la lógica del objetivo principal del programa 
 	public void bloque(JButton h){
 		h.addMouseListener(new java.awt.event.MouseAdapter() { 
 			public void mouseClicked(java.awt.event.MouseEvent e) { 
-				int a=(int) ((3-0+1)*Math.random()+0);
+				int a=(int) ((10-0+1)*Math.random()+0);
 				if(a!=0){
 					h.setBackground(java.awt.Color.yellow);
 				}
@@ -20,5 +21,6 @@ public class ClaseModelo {
 			}
 		}
 				);
+
 	}
 }
