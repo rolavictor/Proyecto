3a0445e536b917810f3391b02b24f8a8bcc7c931
Victor <38298615+rolavictor@users.noreply.github.com>
2018-05-16 11:01:19 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,21 +1,38 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class ClaseModelo {
-	
-	 // Implementa la lógica del objetivo principal del programa 
+
+	// Implementa la lógica del objetivo principal del programa 
 	public void bloque(JButton h){
 		h.addMouseListener(new java.awt.event.MouseAdapter() { 
+			
 			public void mouseClicked(java.awt.event.MouseEvent e) { 
-				int a=(int) ((10-0+1)*Math.random()+0);
+				
+				int a=(int) ((24-0+1)*Math.random()+0);
 				if(a!=0){
 					h.setBackground(java.awt.Color.yellow);
 				}
 				if(a==0) {
 					h.setBackground(java.awt.Color.red);
-					JOptionPane.showMessageDialog(null, "Has perdido");
-		            System.exit(0);
-		          
+					
+
+					int reply=JOptionPane.showConfirmDialog(null, "HAS PERDIDO ¿Quieres volver a jugar?","Alerta", JOptionPane.YES_NO_OPTION);
+					if (reply == JOptionPane.NO_OPTION)
+					{
+						System.exit(0);
+					}else{
+						ClaseModelo modelo = new ClaseModelo(); 
+						ClaseVista vista = new ClaseVista();
+						new ClaseControlador(vista, modelo); 
+					}
 				}
 			}
 		}
