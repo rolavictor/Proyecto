8bb2f65525cf74b2a8c56bba777f3a3b397d0d72
Victor <38298615+rolavictor@users.noreply.github.com>
2018-05-17 09:03:28 +0200

    Update ClaseModelo.java

--- ClaseModelo.java
+++ ClaseModelo.java
@@ -1,21 +1,18 @@
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowListener;
-
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class ClaseModelo {
 
-	// Implementa la lógica del objetivo principal del programa 
+	/**
+	 * @author Noruega
+	 * Cuando se clicka en un botón se genera el botón rojo o el amarillo, 
+	 * después aparece una ventana para seguir jugando o terminar el programa.
+	 * */
 	public void bloque(JButton h){
 		h.addMouseListener(new java.awt.event.MouseAdapter() { 
-			
+
 			public void mouseClicked(java.awt.event.MouseEvent e) { 
-				
+
 				int a=(int) ((24-0+1)*Math.random()+0);
 				if(a!=0){
 					h.setBackground(java.awt.Color.yellow);
@@ -23,10 +20,8 @@ public class ClaseModelo {
 				if(a==0) {
 					h.setBackground(java.awt.Color.red);
 					
-
 					int reply=JOptionPane.showConfirmDialog(null, "HAS PERDIDO ¿Quieres volver a jugar?","Alerta", JOptionPane.YES_NO_OPTION);
-					if (reply == JOptionPane.NO_OPTION)
-					{
+					if (reply == JOptionPane.NO_OPTION){
 						System.exit(0);
 					}else{
 						ClaseModelo modelo = new ClaseModelo(); 
